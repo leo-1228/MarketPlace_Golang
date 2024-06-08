98b678139f02a423d083539ed3f82dc5d656dc89
leo-1228 <akiraito731@gmail.com>
2024-06-07 23:04:20 -0400

    Implement RegisterRoutes function

--- pkg/server/products/products.go
+++ pkg/server/products/products.go
@@ -1,9 +1,34 @@
 package products
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
 // Product dto object for a product .
 type Product struct {
 	ID       int     `json:"id"`
 	Name     string  `json:"name"`
 	Price    float64 `json:"price"`
 	Quantity int     `json:"quantity"`
+}
+
+type contextKey string
+
+var productContextKey = contextKey("product")
+
+// RegisterRoutes applies the routes for the products service.
+func RegisterRoutes(router chi.Router) http.Handler {
+	router.Get("/", ListProducts)
+	router.Post("/", CreateProducts)
+
+	router.Route("/{productId}", func(r chi.Router) {
+		r.Use(ProductContext)
+		r.Get("/", GetProductItem)
+		r.Patch("/", UpdateProductItem)
+		r.Delete("/", DeleteProductItem)
+	})
+
+	return router
 }
