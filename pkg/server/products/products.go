1f3b869936586b09d9920dac8ffed246aa95b003
leo-1228 <akiraito731@gmail.com>
2024-06-07 23:03:19 -0400

    add product struct

---
+++ pkg/server/products/products.go
@@ -0,0 +1,9 @@
+package products
+
+// Product dto object for a product .
+type Product struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+}
